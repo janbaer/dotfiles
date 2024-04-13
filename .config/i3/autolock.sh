ee846bf298535e91f9edf2cd5bf6d60511ce1cb0
Jan Baer <jan@janbaer.de>
2024-04-13 18:53:32 +0200

    i3: Remove support for i3 and X window

--- .config/i3/autolock.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-TIME=${1:-10}
-
-exec xautolock -detectsleep \
-  -time $TIME -locker "~/.config/i3/lock.sh" \
-  -notify 30 \
-  -notifier "notify-send -u critical --icon=dialog-information -t 10000 -- 'AutoLock' 'Screen will be locked in 30 seconds'"
