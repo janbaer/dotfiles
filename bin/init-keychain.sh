8a9259d086b3802d9ed7e983685d920a46573f5b
Jan Baer <jan@janbaer.de>
2025-04-02 17:28:14 +0200

    scripts: Improve init-keychain script to support MacOS

--- bin/init-keychain.sh
+++ bin/init-keychain.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env zsh
 
-# set -x
-
 if [[ -z "${KEYCHAIN_KEYS}" ]]; then
   echo "KEYCHAIN_KEYS not set, skipping keychain"
   exit 1
+fi
+
+if [[ "$(uname)" == "Darwin" ]]; then
+  killall ssh-agent
+  eval "$(ssh-agent)"
 fi
 
 stringList="$KEYCHAIN_KEYS"
@@ -12,7 +15,12 @@ stringList="$KEYCHAIN_KEYS"
 keys=(${(s: :)stringList})
 
 for key in "${keys[@]}"; do
-  eval $(keychain --quiet --agents ssh --eval "$key")
+  if [[ "$(uname)" == "Darwin" ]]; then
+    ssh-add --apple-use-keychain ~/.ssh/$key 2>/dev/null
+  else
+    eval $(keychain --quiet --agents ssh --eval "$key")
+  fi
 done
 
+mkdir -p ~/tmp
 touch ~/tmp/keychain_init_done
