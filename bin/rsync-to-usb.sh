c0b1912cf0e9d75b0d9f8e8e1a96f702bf17d160
Jan Baer <jan@janbaer.de>
2025-04-25 17:03:29 +0200

    scripts: Add /etc/bu folder to local-usb backup

--- bin/rsync-to-usb.sh
+++ bin/rsync-to-usb.sh
@@ -8,3 +8,4 @@ backup "USB" "${hostname}/$(whoami)" "$(whoami)" "/home/$(whoami)/" "/home/$(who
 backup "USB" "${hostname}/local-bin" "$(whoami)" "/usr/local/bin/" "/home/$(whoami)/rsync-local-excludes"
 backup "USB" "${hostname}/etc" "$(whoami)" "/etc/hosts" "/home/$(whoami)/rsync-local-excludes"
 backup "USB" "${hostname}/etc" "$(whoami)" "/etc/fstab" "/home/$(whoami)/rsync-local-excludes"
+backup "USB" "${hostname}/etc/bu" "$(whoami)" "/etc/bu/" "/home/$(whoami)/rsync-local-excludes"
