762b89c7cff5062888f2e2afb7faf1c711bb5ddc
Jan Baer <jan@janbaer.de>
2025-06-25 15:50:54 +0200

    feat(nvim): add vim global to lua lsp diagnostics config
    This is to prevent unnecessary linter warnings

--- .config/nvim/lsp/lua.lua
+++ .config/nvim/lsp/lua.lua
@@ -1,14 +1,17 @@
 ---@type vim.lsp.Config
 return {
-    cmd = { "lua-language-server" },
-    filetypes = { "lua" },
-    root_markers = { ".luarc.json", ".luarc.jsonc" },
-    telemetry = { enabled = false },
-    settings = {
-        Lua = {
-            runtime = {
-                version = "LuaJIT",
-            },
-        },
+  cmd = { "lua-language-server" },
+  filetypes = { "lua" },
+  root_markers = { ".luarc.json", ".luarc.jsonc" },
+  telemetry = { enabled = false },
+  settings = {
+    Lua = {
+      runtime = {
+        version = "LuaJIT",
+      },
+      diagnostics = {
+        globals = { "vim" },
+      },
     },
+  },
 }
