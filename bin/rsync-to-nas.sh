d803a343366f66b91d364944cfe972fe6eac961f
Jan Baer <jan@janbaer.de>
2026-02-10 19:30:29 +0100

    dotfiles: Remove scripts and configs that are no longer used

--- bin/rsync-to-nas.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-. /home/jan/bin/rsync-mydata.sh
-
-hostname=$(hostname | tr '[a-zA-Z]' '[A-Za-z]')
-
-backup "jabasoft-ug" "${hostname}/$(whoami)" "$(whoami)" "/home/$(whoami)/" "/home/$(whoami)/rsync-remote-excludes"
